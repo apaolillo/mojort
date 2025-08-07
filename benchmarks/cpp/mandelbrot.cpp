874c28d80c0e4fd3ef262e85ae80d1560b300634
Aaron Bogaert <aabogaer@soft.vub.ac.be>
2025-08-07 15:38:18 +0200

    Add mandelbrot benchmark in Mojo and C++
    In this benchmark we compare runtime, jitter and file size.
    
    We will later add a check for correctness.

---
+++ benchmarks/cpp/mandelbrot.cpp
@@ -0,0 +1,86 @@
+// https://github.com/dario-marvin/Mandelbrot/blob/master/Mandelbrot.cc
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <complex>
+#include <cmath>
+
+#include <chrono>
+
+using namespace std;
+typedef complex<double> fcomp;
+
+int mandelbrotComplex(fcomp c) {
+	int limit = 1000;
+	fcomp z = c;
+
+	for (int i = 0; i < limit; ++i) {
+		if (abs(z) > 2) return i;
+		z = z * z + c;
+	}
+	return limit;
+}
+
+int mandelbrotSplit(double real, double imag) {
+	int limit = 1000;
+	double zReal = real;
+	double zImag = imag;
+
+	for (int i = 0; i < limit; ++i) {
+		double r2 = zReal * zReal;
+		double i2 = zImag * zImag;
+
+		if (r2 + i2 > 4.0) return i;
+
+		zImag = 2.0 * zReal * zImag + imag;
+		zReal = r2 - i2 + real;
+	}
+	return limit;
+}
+
+
+int main(int argc, char *argv[]){
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+
+	int width = atol(argv[1]); //number of characters fitting horizontally on my screen
+	int heigth = width; //number of characters fitting vertically on my screen
+
+	double x_start = -2.0;
+	double x_fin = 0.47;
+	double y_start = -1.12;
+	double y_fin = 1.12;
+
+	double dx = (x_fin - x_start)/(width - 1);
+	double dy = (y_fin - y_start)/(heigth - 1);
+
+    auto t1 = high_resolution_clock::now();
+    auto a = 0;
+	for (int i = 0; i < heigth; i++) {
+		for (int j = 0; j < width; j++) {
+
+			double x = x_start + j*dx; // current real value
+			double y = y_fin - i*dy; // current imaginary value
+
+			// cout << mandelbrotComplex(fcomp(x,y)) << '\n';
+			// cout << mandelbrotSplit(x,y) << '\n';
+
+            a = a + mandelbrotComplex(fcomp(x,y));
+            // a = a + mandelbrotSplit(x,y);
+        }
+	}
+    auto t2 = high_resolution_clock::now();
+
+    /* Getting number of milliseconds as a double. */
+    duration<double, std::milli> ms_double = t2 - t1;
+    // printing a otherwise the compiler will optimize the loop away
+    std::cout << int(a) << "\n";
+    std::cout << "runtime: " << int((ms_double.count() * 1000)) << " µs\n";
+    return 0;
+
+	return 0;
+}
---
+++ benchmarks/mojo/mandelbrot.mojo
@@ -0,0 +1,55 @@
+from complex import ComplexFloat64, abs
+from sys.arg import argv
+import time
+
+alias MAX_ITERS = 1000
+
+fn mandelbrot_complex(c: ComplexFloat64) -> Int:
+    var z = c
+    var nv = 0
+    for _ in range(0, MAX_ITERS):
+      if abs(z) > 2:
+        break
+      z = z*z + c
+      nv += 1
+    return nv
+
+fn mandelbrot_split(real:Float64, imag:Float64) -> Int:
+  var nv = 0
+  var zReal:Float64 = real
+  var zImag:Float64 = imag
+  for _ in range(0, MAX_ITERS):
+    var r2:Float64 = zReal * zReal
+    var i2:Float64 = zImag * zImag
+    if r2 + i2 > 4.0:
+      break
+    zImag = 2.0 * zReal * zImag + imag
+    zReal = r2 - i2 + real
+  return nv
+
+alias min_x = -2.0
+alias max_x = 0.47
+alias min_y = -1.12
+alias max_y = 1.12
+
+def main():
+  # arguments
+  # 1) size
+  var width:Int = atol(argv()[1])
+  var height = width
+  var scale_x = (max_x - min_x) / (width - 1)
+  var scale_y = (max_y - min_y) / (height - 1)
+  var a = 0
+
+  var t1 = time.monotonic() / 1000
+  for h in range(height):
+      var cy = max_y - h * scale_y
+      for w in range(width):
+          var cx = min_x + w * scale_x
+          a =  a + mandelbrot_complex(ComplexFloat64(cx,cy))
+          # a =  a + mandelbrot_split(cx,cy)
+  var t2 = time.monotonic() / 1000
+  var diff = (t2 -t1)
+  # printing a otherwise the compiler will optimize the loop away
+  print(a)
+  print(String("runtime: {} µs").format(String(diff)))
