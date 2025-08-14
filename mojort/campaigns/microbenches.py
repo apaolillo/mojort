abfd1579a142a8aca2def8e8ba269977f1cdc373
Antonio Paolillo <apaolill@gmail.com>
2025-08-15 00:22:31 +0200

    Results for microbenches when nb_runs=10
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- mojort/campaigns/microbenches.py
+++ mojort/campaigns/microbenches.py
@@ -11,7 +11,7 @@ from benchkit.commandwrappers.taskset import TasksetWrap
 from benchkit.benchmark import CommandWrapper
 
 
-nb_runs = 20
+nb_runs = 10
 master_thread_core = [11]
 languages = [
     # --- C++ GCC
