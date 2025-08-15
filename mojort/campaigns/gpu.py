cb65fe4d0c010f098e0f599c1d255495c51e770f
Antonio Paolillo <paolillo@soft.vub.ac.be>
2025-08-15 10:07:25 +0200

    Update GPU benchmark
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- mojort/campaigns/gpu.py
+++ mojort/campaigns/gpu.py
@@ -4,9 +4,11 @@ import numpy as np
 from mojort.platforms import get_mojort_docker_platform_from
 from mojort.runners import get_mojort_runner, get_mojort_builder
 from pythainer.examples.runners import gpu_runner
-import pandas as pd
 from benchkit.campaign import CampaignCartesianProduct
 from mojort.benchmarks.matmul_gpu import MatmulGpu
+
+
+CHART_ENABLED = False
 
 
 def main() -> None:
@@ -15,11 +17,11 @@ def main() -> None:
     runner |= gpu_runner()
     platform = get_mojort_docker_platform_from(runner=runner)
     campaign = CampaignCartesianProduct(
-        name="01_latency",
+        name="01_gpu",
         benchmark=MatmulGpu(platform=platform),
-        nb_runs=3,
+        nb_runs=30,
         variables={
-            "size": [16, 32],
+            "size": [256, 512],
             "language": [
                 "mojo"
             ],
@@ -35,6 +37,9 @@ def main() -> None:
 
     # NOTE: there are mistakes made by one of the langaues as both do not produce the exact same output
     # this might be worth mentioning in the paper
+
+    if not CHART_ENABLED:
+        return
 
     def test1(dataframe):
         for lan in dataframe["src_filename"].unique():
