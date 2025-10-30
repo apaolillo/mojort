23a3bff7a1dea18d4e83a25e0a21f5837319af9e
aaronbog <48908435+aaronbog@users.noreply.github.com>
2025-10-30 13:09:00 +0100

    Changed size benchmark to use cargo (#2)
    Signed-off-by: Bogaert Aaron <aaron.bogaert@vub.be>

--- mojort/benchmarks/size.py
+++ mojort/benchmarks/size.py
@@ -2,7 +2,7 @@ import re
 from pathlib import Path
 import subprocess
 from typing import Any, Dict, List, Iterable
-from mojort.utils import language2cmdline, language2foldername
+from mojort.utils import language2cmdline, language2foldername, rust_add_build_path
 from benchkit.benchmark import Benchmark, RecordResult, CommandWrapper
 from benchkit.platforms import Platform
 from benchkit.utils.dir import gitmainrootdir
@@ -37,6 +37,7 @@ class SizeBench(Benchmark):
         **kwargs,
     ) -> None:
         language_folder = language2foldername(language)
+        language_folder = rust_add_build_path(language,src_filename,language_folder)
 
         lg_bench_dir = self._benchmark_dir / language_folder
         if not self.platform.comm.isdir(path=lg_bench_dir):
@@ -48,65 +49,65 @@ class SizeBench(Benchmark):
         match language:
             # --- C++ GCC
             case "cpp -Ofast":
-                cmd = f"g++ -Ofast {src_filename}.cpp -c -o {src_filename}"
+                cmd = f"g++ -Ofast {src_filename}.cpp -c -o {src_filename}.o"
             case "cpp-gcc -O3":
-                cmd = f"g++ -O3 {src_filename}.cpp -c -o {src_filename}"
+                cmd = f"g++ -O3 {src_filename}.cpp -c -o {src_filename}.o"
             case "cpp-gcc -O2":
-                cmd = f"g++ -O2 {src_filename}.cpp -c -o {src_filename}"
+                cmd = f"g++ -O2 {src_filename}.cpp -c -o {src_filename}.o"
             case "cpp-gcc -O1":
-                cmd = f"g++ -O1 {src_filename}.cpp -c -o {src_filename}"
+                cmd = f"g++ -O1 {src_filename}.cpp -c -o {src_filename}.o"
             case "cpp-gcc":
-                cmd = f"g++ {src_filename}.cpp -c -o {src_filename}"
+                cmd = f"g++ {src_filename}.cpp -c -o {src_filename}.o"
 
             # --- C++ Clang
             case "cpp-clang -O3":
-                cmd = f"clang++ -O3 {src_filename}.cpp -c -o {src_filename}"
+                cmd = f"clang++ -O3 {src_filename}.cpp -c -o {src_filename}.o"
             case "cpp-clang -O2":
-                cmd = f"clang++ -O2 {src_filename}.cpp -c -o {src_filename}"
+                cmd = f"clang++ -O2 {src_filename}.cpp -c -o {src_filename}.o"
             case "cpp-clang -O1":
-                cmd = f"clang++ -O1 {src_filename}.cpp -c -o {src_filename}"
+                cmd = f"clang++ -O1 {src_filename}.cpp -c -o {src_filename}.o"
             case "cpp-clang":
-                cmd = f"clang++ {src_filename}.cpp -c -o {src_filename}"
+                cmd = f"clang++ {src_filename}.cpp -c -o {src_filename}.o"
 
             # --- C GCC
             case "c-gcc -O3":
-                cmd = f"gcc -O3 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}"
+                cmd = f"gcc -O3 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}.o"
             case "c-gcc -O2":
-                cmd = f"gcc -O2 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}"
+                cmd = f"gcc -O2 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}.o"
             case "c-gcc -O1":
-                cmd = f"gcc -O1 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}"
+                cmd = f"gcc -O1 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}.o"
             case "c-gcc":
-                cmd = f"gcc -std=c11 -D_GNU_SOURCE {src_filename}.c -lm -c -o {src_filename}"
+                cmd = f"gcc -std=c11 -D_GNU_SOURCE {src_filename}.c -lm -c -o {src_filename}.o"
 
             # --- C Clang
             case "c-clang -O3":
-                cmd = f"clang -O3 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}"
+                cmd = f"clang -O3 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}.o"
             case "c-clang -O2":
-                cmd = f"clang -O2 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}"
+                cmd = f"clang -O2 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}.o"
             case "c-clang -O1":
-                cmd = f"clang -O1 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}"
+                cmd = f"clang -O1 -std=c11 -D_GNU_SOURCE {src_filename}.c -c -lm -o {src_filename}.o"
             case "c-clang":
-                cmd = f"clang -std=c11 -D_GNU_SOURCE {src_filename}.c -lm -c -o {src_filename}"
+                cmd = f"clang -std=c11 -D_GNU_SOURCE {src_filename}.c -lm -c -o {src_filename}.o"
 
             # --- Rust
             case "rust -O3":
-                cmd = f"rustc --emit obj -C opt-level=3 {src_filename}.rs -o {src_filename}"
+                cmd = f"cargo rustc --profile d -- --emit obj={src_filename}.o"
             case "rust -O2":
-                cmd = f"rustc --emit obj -C opt-level=2 {src_filename}.rs -o {src_filename}"
+                cmd = f"cargo rustc --profile b -- --emit obj={src_filename}.o"
             case "rust -O1":
-                cmd = f"rustc --emit obj -C opt-level=1 {src_filename}.rs -o {src_filename}"
+                cmd = f"cargo rustc --profile c -- --emit obj={src_filename}.o"
             case "rust":
-                cmd = f"rustc --emit obj {src_filename}.rs -o {src_filename}"
+                cmd = f"cargo rustc --profile a -- --emit obj={src_filename}.o"
 
             # --- Mojo
             case "mojo":
-                cmd = f"{mojo_bin}/mojo build -O 0 --debug-level none --emit object -o {src_filename} {src_filename}.mojo"
+                cmd = f"{mojo_bin}/mojo build -O 0 --debug-level none --emit object -o {src_filename}.o {src_filename}.mojo"
             case "mojo -O1":
-                cmd = f"{mojo_bin}/mojo build -O 1 --debug-level none --emit object -o {src_filename} {src_filename}.mojo"
+                cmd = f"{mojo_bin}/mojo build -O 1 --debug-level none --emit object -o {src_filename}.o {src_filename}.mojo"
             case "mojo -O2":
-                cmd = f"{mojo_bin}/mojo build -O 2 --debug-level none --emit object -o {src_filename} {src_filename}.mojo"
+                cmd = f"{mojo_bin}/mojo build -O 2 --debug-level none --emit object -o {src_filename}.o {src_filename}.mojo"
             case "mojo -O3":
-                cmd = f"{mojo_bin}/mojo build -O 3 --debug-level none --emit object -o {src_filename} {src_filename}.mojo"
+                cmd = f"{mojo_bin}/mojo build -O 3 --debug-level none --emit object -o {src_filename}.o {src_filename}.mojo"
 
         self.platform.comm.shell(
             command=cmd,
@@ -124,12 +125,13 @@ class SizeBench(Benchmark):
 
         src_filename: str = build_variables["src_filename"]
         lg_bench_dir = self._benchmark_dir / language_folder
+        lg_bench_dir = rust_add_build_path(language,src_filename,lg_bench_dir)
 
         # command to analyze the file size
-        cmd = ["du","-b",f"./{src_filename}",]
+        cmd = ["du","-b",f"./{src_filename}.o",]
 
         # strip the file
-        self.platform.comm.shell(f"strip -s ./{src_filename}",current_dir=lg_bench_dir)
+        self.platform.comm.shell(f"strip -s ./{src_filename}.o",current_dir=lg_bench_dir)
 
         # added command to get the object dump for debugging
         # self.platform.comm.shell(f'objdump -d ./{src_filename} > ./{src_filename}.dmp',shell = True,current_dir=lg_bench_dir)
